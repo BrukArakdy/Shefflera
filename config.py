7e8829fb559e8e9c58ce9558e942f26f6896ad1d
Arkady Bruk <bruk1937@gmail.com>
2017-11-22 14:49:46 +0300

    Change paths

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ WEBHOOK_HOST = '104.131.96.14'
 WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
 WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 
-WEBHOOK_SSL_CERT = '/home/gorec/test/db_and_certs/webhook_cert.pem'  # Path to the ssl certificate
-WEBHOOK_SSL_PRIV = '/home/gorec/test/db_and_certs/webhook_pkey.pem' # Path to the ssl private key
+WEBHOOK_SSL_CERT = '/home/gorec/Shefflera/db_and_certs/webhook_cert.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = '/home/gorec/Shefflera/db_and_certs/webhook_pkey.pem' # Path to the ssl private key
 
 WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
 WEBHOOK_URL_PATH = "/%s/" % (API_TOKEN)
 
-DB = "/home/gorec/test/db_and_certs/Shefflera.db"
+DB = "/home/gorec/Shefflera/db_and_certs/Shefflera.db"
 
 CONTACTS_MAPPING = {
     'VK': 'Мы в VK: https://vk.com/shefflera_organic',
